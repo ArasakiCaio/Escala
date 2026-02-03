db5c91772a967358433d2c23789f0d6935d4b116
Caio Arasaki <sasazaki08@gmail.com>
2026-02-03 14:27:50 -0300

    feat: dicionário dos dias criado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var dias = new Dictionary<int, string>
+{
+    { 1, "Segunda"},
+    { 2, "Terça"},
+    { 3, "Quarta" },
+    { 4, "Quinta" },
+    { 5, "Sexta" },
+    { 6 , "Sábado" },
+    { 7, "Domingo" }
+};
+
+for (int i = 1; i<8; i++)
+{
+    Console.WriteLine($"{dias[i]}");
+}
