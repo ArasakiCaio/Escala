c9616c8f77253df1bc01456ed1058eac0d47e9a3
Caio Arasaki <sasazaki08@gmail.com>
2026-02-03 15:00:03 -0300

    feat: classes das missas criadas

--- Program.cs
+++ Program.cs
@@ -9,7 +9,45 @@
     { 7, "Domingo" }
 };
 
-for (int i = 1; i<8; i++)
+abstract class Missa
 {
-    Console.WriteLine($"{dias[i]}");
+    public int Id { get; set; }
+    
+    public required string Dia { get; set; }
+    public required string Data { get; set; }
+    public required string Horario { get; set; }
+    public abstract int[] Acolitos { get; set; }
+    public abstract int[] Coroinhas {get; set;}
+}
+
+class MissaSemana : Missa
+{
+    public override required int[] Acolitos { get; set;}
+    public override required int[] Coroinhas { get; set;}
+
+    public MissaSemana(int id, string dia, string data, string horario, int acolito, int[] coroinhas)
+    {
+        Id = id;
+        Dia = dia;
+        Data = data;
+        Horario = horario;
+        Acolitos = new int[] { acolito };
+        Coroinhas = coroinhas;
+    }
+}
+
+class MissaFimSemana : Missa
+{
+    public override required int[] Acolitos { get; set;}
+    public override required int[] Coroinhas { get; set;}
+
+    public MissaFimSemana(int id, string dia, string data, string horario, int[] acolitos, int[] coroinhas)
+    {
+        Id = id;
+        Dia = dia;
+        Data = data;
+        Horario = horario;
+        Acolitos = acolitos;
+        Coroinhas = coroinhas;
+    }
 }
