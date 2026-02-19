6f108a49a6b7fd037ef0f61318c59efefe161c50
ArasakiCaio <sasazaki08@gmail.com>
2026-02-19 14:39:35 -0300

    feat: Classe de integrante criada e função de ler a planilha criada

--- Integrantes.cs
+++ Integrantes.cs
@@ -1,5 +1,3 @@
-using Semanas;
-
 namespace Integrantes
 {
     class Integrante
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Semanas;
-
+using Integrantes;
 class Program
 {
 
@@ -29,6 +29,52 @@ class Program
         { 6, "Sábado" },
         { 7, "Domingo" }
     };
+
+    public static int[] StringToIntArray(string[] input)
+    {
+        List<int> result = new List<int>();
+        foreach (string str in input)
+        {
+            result.Add(Convert.ToInt32(str));
+        }
+
+        return result.ToArray();
+    }
+
+    public static void LerPlanilha(string File)
+    {
+        var data = new StreamReader(File);
+        List<Integrante> Coroinhas = new List<Integrante>();
+        List<Integrante> Acolitos = new List<Integrante>();
+
+        data.ReadLine();
+
+        while (!data.EndOfStream)
+        {
+            var line = data.ReadLine();
+            if (line != null)
+            {
+                var values = line.Split(',');
+                if(values[1] == "Coroinha")
+                {
+                    Coroinhas.Add(new Integrante(Convert.ToInt32(values[0]), values[2], Convert.ToBoolean(values[3]), StringToIntArray(values[4].Split(';')), StringToIntArray(values[5].Split(';')), new int[5], new int[3], new int[3]));
+                }
+                else if(values[1] == "Acolito")
+                {
+                    Acolitos.Add(new Integrante(Convert.ToInt32(values[0]), values[2], Convert.ToBoolean(values[3]), StringToIntArray(values[4].Split(';')), new int[1], StringToIntArray(values[5].Split(';')), new int[3], new int[3]));
+                }
+            }
+        }
+
+        foreach (var val in Coroinhas)
+        {
+            Console.WriteLine($"Coroinha - {val.Id}: {val.Nome}");
+        }
+        foreach (var val in Acolitos)
+        {
+            Console.WriteLine($"Acolito - {val.Id}: {val.Nome}");
+        }
+    }
 
     public static Semana[] CriarMes()
     {
@@ -92,6 +138,7 @@ class Program
 
     static void Main()
     {
-        Semana[] Mes = CriarMes();
+        // Semana[] Mes = CriarMes();
+        // LerPlanilha("Data.csv");
     }
 }
